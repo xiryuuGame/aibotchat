86b9153bb91e2fd26fb250697a669c0160193d0f
xiryuuGame <farrel2008ganteng@gmail.com>
2025-03-24 23:37:08 +0700

    make a new function

---
+++ function/groupinformation.js
@@ -0,0 +1,16 @@
+async function gf(sock, groupId) {
+  if (typeof groupId !== "string") {
+    return "error: groupId tidak valid";
+  }
+  if (!groupId.endsWith("@g.us")) {
+    return "error: saat ini user tidak sedang berada dalam group";
+  }
+  try {
+    const metadata = await sock.groupMetadata(groupId);
+    return JSON.stringify(metadata, null, 2); // Mengubah objek ke string JSON dengan indentasi dan spasi;
+  } catch (error) {
+    console.error("Error fetching group metadata:", error);
+    return "Failed to fetch group metadata.";
+  }
+}
+module.exports = gf;
